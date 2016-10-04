35c63c82b7f51817aad47c515b12cba0f78e30af
er860511 <er860511@DESKTOP-CPRQNVT>
2016-10-05 01:20:46 +0800

    hwf

---
+++ src/hwf/hwf.java
@@ -0,0 +1,11 @@
+package hwf;
+
+public class hwf {
+	public static void main(String[] args) {
+		String lyrics="Let it go! Let it go! Cannot hold it back anymore";
+			lyrics=lyrics.replaceAll("it","her");
+			lyrics=lyrics.replaceAll("Cannot","Can't");
+			System.out.println(lyrics);
+
+	}
+}
